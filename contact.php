93ae530a4a806e83aaf29b817629a046f8b87435
huzefatai <40890238+huzefatai@users.noreply.github.com>
2018-08-14 12:58:10 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+ <head>
+	 <title>Untitled</title>
+	 <meta charset="UTF-8"/>
+ 		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+ <style>
+ * {
+ margin:0;
+ padding:0;
+ }
+ 
+ 
+ body{
+	margin: 0;
+	padding:0;
+	font-family: 'PT' , sans-serif;
+	background-color: white;
+	user-select: none;
+}
+
+.container {
+background-color: 	rgba(255, 255, 255, .5);
+width: 106%;
+height: 0%;
+padding: 0px 0px 7em 0px;
+position: relative;
+box-sizing: border-box;
+box-shadow: 0px 5px 5px 5px rgba(0, 0, 0, .1);
+box-shadow: 0px 20px 50px 10px 100px rgba(0, 0, 0, .1);
+}
+
+.logo {
+padding: 0px 0px;
+font-size: 1.3em;
+color: #4d94ff;
+float: left;
+margin-top: 33px;
+margin-left: 3em;
+letter-spacing: 2.5px;
+}
+
+nav {
+position: relative;
+float: right;
+margin: 2.8em 2em 0px 0em;
+}
+
+nav ul {
+margin: 0;
+padding: 0;
+display: flex;
+}
+
+nav ul li {
+list-style-type: none;
+}
+
+nav ul li a {
+text-decoration: none;
+font-size: 1.1em;
+padding: 0px 20px;
+color: white;
+text-transform: uppercase;
+color: black;
+transition: .2s;
+letter-spacing: 2.5px;
+}
+
+nav ul li ul {
+display: block;
+position: absolute;
+background: #867979;
+border-radius: 1em;
+opacity: 0;
+visibility: hidden;
+transition: .2s ease-in-out;
+margin-top: 1em;
+padding: 30px;
+line-height: 2em;
+}
+
+nav ul li ul li a {
+letter-spacing: 5px;
+font-size: 1.3em;
+padding: 0px;
+color: white;
+transition: .2s ease-in-out;
+}
+
+.active {
+color: gray;
+}
+
+nav ul li ul li a:hover
+{
+
+color: lightgray;
+
+}
+
+
+nav ul li a:hover {
+color: gray;
+}
+
+
+nav ul li:hover ul {
+opacity: 1;
+visibility: visible;
+}
+
+ 
+ form {
+ background-color: rgba(255,255,255,0.5);
+ padding: 1.8em;
+ width: 100%;
+ height: 100%
+ box-sizing: border-box;
+ box-shadow: 0px 5px 5px  rgba(0,0,0,.1);
+ text-align: center;
+ margin: 10px 0px 0px 0px;
+ } 
+ 
+ .headline h1 
+ {
+
+font-size: 4em;
+font-weight: 200;
+ top: 0;
+ padding: 0px 0px 50px 0px ;
+ letter-spacing: 5px
+ margin: 0px 0px 10px 0px;
+ font-family: 'PT Sans', sans-serif;
+ }
+ 
+ form input {
+ user-select: none;
+ font-size: 1.2em;
+ box-sizing: border-box;
+ border: none;
+ width: 100%;
+ padding: 25px 0px 25px 55px;
+ border: 2px solid rgba(0,0,0,.1);
+ outline: none;
+ margin: 0px 0px 20px 0px;
+ transition: .2s;
+ }
+ 
+form input:focus + i {
+ color: dodgerblue;
+ transition: .2s;
+ }
+ 
+ ::placeholder {
+ text-transform: uppercase;
+ }
+ 
+ button{
+ width: 100%;
+ font-size: 2em;
+ text-transform: uppercase;
+ box-sizing: border-box;
+ border: none;
+ width: 100%;
+ padding: 15px;
+ border: 2px solid rgba(0,0,0,.1);
+ outline: none;
+ margin: 0px 0px 10px 0px;
+ }
+ 
+ .user i {
+ position: absolute;
+ font-size: 24px; 
+ margin: -3em 0em 0em -19.5em;
+ }
+ 
+ .email i {
+ position: absolute;
+ font-size: 24px; 
+ margin: -3em 0em 0em -19.5em;
+ }
+ 
+ .textarea i {
+ position: absolute;
+ font-size: 24px; 
+ margin: -3em 0em 0em -19.5em;
+ }
+ 
+ form{
+ padding-top: 100px
+ }
+
+ </style>
+ </head>
+	 <body>
+	
+<doiv class= "container">
+		<div  class="logo"><h1>Yogic Way</h1></div>
+		<nav>
+		<ul>
+		
+		<li><a href="index.php">Home</a></li>
+		<li><a href="#">Register</a>
+		<ul>
+		<li><a href="student_reg.php">Student</a></li>
+		<li><a href="tr_reg.html">Trainer</a></li>
+		</ul>
+		</li>
+		<li><a href="login.php">Login</a></li>
+		<li><a href="contact.php" class="active">Contact Us</a></li>
+		</ul>
+		</nav
+				</div>
+			</div>	
+	
+	<form>
+	<div class="headline">
+	<h1>Contact Form</h1>
+	</div>
+	<form>
+	<div class="user">
+			<input  type="text" name="name" placeholder="Name" required>
+			<i class="fas fa-user"></i>
+			</div>
+		 	
+		 	<div class="email">
+		 <input type="email" name="email" placeholder="E-mail" required>
+		 <i class="fas fa-envelope"></i>
+		 </div>
+		  <div class="textarea">
+		 	<input type="textarea" name="text" placeholder="Message" required>
+		 	<i class="fas fa-comments"></i>
+		 	</div>		 
+		 	
+		 		<button type="submit" value="submit">Submit</button>
+	</form>
+	 </body>
+ </html>
---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$dbservername = 'localhost';	
+$dbusername = 'root';
+$dbpassword = '7sSeASYCCRKDa3tW';
+$dbname = 'sign up';
+
+$conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
