55af416c9e7e02c99f250973ea876c6e592df9fd
huzefatai <40890238+huzefatai@users.noreply.github.com>
2018-09-20 19:31:33 +0530

    Delete db.php

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbservername = 'localhost';	
-$dbusername = 'root';
-$dbpassword = '7sSeASYCCRKDa3tW';
-$dbname = 'sign up';
-
-$conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
